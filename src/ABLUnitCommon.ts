93b642229566f4bf5b1fd93be13c3b8594a8444a
kenherring <kennethherring@gmail.com>
2024-01-13 16:36:51 -0500

    Ensure timely response when cancelling a test run

--- src/ABLUnitCommon.ts
+++ src/ABLUnitCommon.ts
@@ -10,52 +10,94 @@ logOutputChannel.clear()
 
 class Logger {
 	info (message: string, testRun?: TestRun) {
-		message = this.decorateMessage(message)
-		this.logTestConsole(message, testRun)
+		message = this._decorateMessage(message)
+		this._logTestConsole(message, testRun)
 		console.log(message)
 		logOutputChannel.info(message)
 	}
 	warn (message: string, testRun?: TestRun) {
-		message = this.decorateMessage(message)
-		this.logTestConsole(message, testRun)
+		message = this._decorateMessage(message)
+		this._logTestConsole(message, testRun)
 		console.warn(message)
 		logOutputChannel.warn(message)
 	}
 	debug (message: string, testRun?: TestRun) {
-		message = this.decorateMessage(message)
-		this.logTestConsole(message, testRun)
+		message = this._decorateMessage(message)
+		this._logTestConsole(message, testRun)
 		console.debug(message)
 		logOutputChannel.debug(message)
 	}
 	trace (message: string, testRun?: TestRun) {
-		message = this.decorateMessage(message)
-		this.logTestConsole(message, testRun)
+		message = this._decorateMessage(message)
+		this._logTestConsole(message, testRun)
 		console.trace(message)
 		logOutputChannel.trace(message)
 	}
-	error (message: string, testRun?: TestRun) {
-		message = this.decorateMessage(message)
-		this.logTestConsole(message, testRun)
+	error (message: string | Error, testRun?: TestRun) {
+		if (message instanceof Error) {
+			if (message.stack) {
+				message = '[' + message.name + '] ' +  message.message + "\r\r" + message.stack
+			} else {
+				message = '[' + message.name + '] ' +  message.message
+			}
+		}
+		message = this._decorateMessage(message)
+		this._logTestConsole(message, testRun)
 		console.error(message)
 		logOutputChannel.error(message)
 	}
 
-	private decorateMessage (message: string) {
-		return this.getPrefix() + ' ' + message
+	private _getCallerSourceLine () {
+		const prepareStackTraceOrg = Error.prepareStackTrace
+		const err = new Error()
+		Error.prepareStackTrace = (_, stack) => stack
+		const stack = err.stack as unknown as NodeJS.CallSite[]
+		Error.prepareStackTrace = prepareStackTraceOrg
+
+		let ret: string | undefined = undefined
+		for (const s of stack) {
+			const fileName = s.getFileName()
+			if (fileName && fileName !== __filename) {
+				ret = fileName.replace(path.normalize(__dirname),'').substring(1).replace(/\\/g, '/')
+				ret = ret + ' ' + s.getFunctionName() + ':' + s.getLineNumber()
+				return ret
+			}
+		}
 	}
 
-	private logTestConsole (message: string, testRun: TestRun | undefined) {
+	private _decorateMessage (message: string) {
+		const callerSourceLine = this._getCallerSourceLine()
+		if (callerSourceLine) {
+			return '[' + callerSourceLine + '] ' + message
+		}
+		return message
+	}
+
+	private _logTestConsole (message: string, testRun: TestRun | undefined) {
 		if (!testRun) { return }
 		const optMsg = message.replace(/\r/g, '').replace(/\n/g, '\r\n')
 		testRun.appendOutput(optMsg + "\r\n")
 	}
 
-	private getPrefix () {
-		return '[' + path.normalize(__dirname + "/..").replace(/\\/g, '/') + ']'
-	}
+	// getPrefix () {
+	// 	let source = path.normalize(__filename)
+	// 	source = source.replace(path.normalize(__dirname),'')
+	// 	source = source.substring(1)
+	// 	return '[' + source + ']'
+	// 	// return '[' + path.normalize(__filename).replace(/\\/g, '/') + ']'
+	// }
 }
 
-export const log = new Logger()
+export const getPrefix = () => {
+	let source = path.normalize(__filename)
+	source = source.replace(path.normalize(__dirname),'')
+	source = source.substring(1)
+	return '[' + source + ']'
+}
+
+export const log: Logger = new Logger()
+
+// module.exports = new Logger()
 
 export const readStrippedJsonFile = (uri: Uri | string): JSON => {
 	if (typeof uri === 'string') {
